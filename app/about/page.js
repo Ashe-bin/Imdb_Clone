98516af31b4866723e27cb7289f4def0ec9494b7
Ashe-bin <118310102+Ashe-bin@users.noreply.github.com>
2024-10-17 12:54:02 +0300

    create about page

---
+++ app/about/page.js
@@ -0,0 +1,27 @@
+import React from "react";
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="'text-2xl font-medium text-amber-600 ">About </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sit
+        similique, itaque earum recusandae atque dolore aliquid autem officia
+        non accusamus officiis vitae, doloribus vero obcaecati, repellat quod
+        laudantium. Vitae.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In praesentium
+        consequuntur autem quo aliquam ea tenetur soluta iure quae expedita,
+        quis eaque dolor quam sapiente laboriosam repellat obcaecati
+        reprehenderit impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+        soluta velit nobis dicta repellat tenetur similique officiis cupiditate
+        dolore mollitia! Aspernatur, eveniet asperiores distinctio nisi corrupti
+        obcaecati adipisci consequuntur non.
+      </p>
+    </div>
+  );
+};
+export default About;
