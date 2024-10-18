90605805dbcc5ff7f85138c3792a2a156a414cd5
Ashe-bin <118310102+Ashe-bin@users.noreply.github.com>
2024-10-18 14:01:56 +0300

    creating movie card

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import loading from "./loading";
+import Card from "@/components/Card";
 
 const API_KEY = process.env.API_KEY;
 
@@ -60,11 +61,11 @@ function Home({ searchParams }) {
   return (
     <div>
       {results.length > 0 ? (
-        results.map((result) => (
-          <div key={result.id}>
-            <h2>{result.original_title}</h2>
-          </div>
-        ))
+        <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
+          {results.map((result) => (
+            <Card key={result.id} result={result} />
+          ))}
+        </div>
       ) : (
         <p>No results found.</p>
       )}
